80537fb95ae799f8d8c4d2058a63fd850ca454fa
winniewandia <winniewandia63@gmail.com>
2023-07-24 08:59:49 +0300

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t n, i, temp;
+
+	if (size < 2)
+	{
+		print_array(array, size);
+		return;
+	}
+	for (n = 0; n < size; n++)
+	{
+		for (i = 0; i < size - 2; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Bubble sort
+Repeat n-1 times:
+for i from 0 to n-2:
+if no[i] and no[i+1] out of order:
+swap them
+if no swaps:
+Quit
